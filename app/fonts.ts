9bc7616e9ee2d8f142631ba9bd775e4d4a7714eb
DESKTOP-MAF5T6N\Darky <anandarizky166@gmail.com>
2024-01-26 14:32:04 +0700

    setup google fonts in nextjs

---
+++ app/fonts.ts
@@ -0,0 +1,15 @@
+import { Montserrat, Roboto } from 'next/font/google';
+
+export const montserrat = Montserrat({
+	weight: ['400', '700'],
+	style: ['normal', 'normal'],
+	subsets: ['latin'],
+	display: 'swap',
+});
+
+export const roboto = Roboto({
+	weight: ['400', '700'],
+	style: ['normal', 'normal'],
+	subsets: ['latin'],
+	display: 'swap',
+});
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next';
 import './globals.css';
+import { montserrat, roboto } from './fonts';
 
 
 export const metadata: Metadata = {
@@ -14,7 +15,7 @@ export default function RootLayout({
 }>) {
 	return (
 		<html lang="en">
-			<body>{children}</body>
+			<body className={montserrat.className}>{children}</body>
 		</html>
 	);
 }
