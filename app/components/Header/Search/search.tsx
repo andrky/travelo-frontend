e4786abcaa81665e4986ed378ff5c1d5fad3eef9
DESKTOP-MAF5T6N\Darky <anandarizky166@gmail.com>
2024-01-27 12:10:38 +0700

    search component

---
+++ app/components/Header/Search/search.tsx
@@ -0,0 +1,17 @@
+import Image from 'next/image';
+
+import searchIcon from '../../../../public/assets/icons/search-icon.svg';
+
+const Search = () => {
+	return (
+		<form className="flex item-center ml-8">
+			<input type="text" placeholder="Kategori    |Lokasi" className="border border-gray-50 py-3 px-3 rounded-l-md text-sm text-gray-50" />
+
+			<button className="flex justify-center items-center bg-blue-100 border-blue-100 h-[50px] w-[50px] text-white text-sm rounded-r-md" type="submit">
+				<Image src={searchIcon} alt="Search Icon" height="24" width="24" />
+			</button>
+		</form>
+	);
+};
+
+export default Search;
--- app/components/Header/header.tsx
+++ app/components/Header/header.tsx
@@ -1,15 +1,19 @@
 import Image from 'next/image';
 
 import Container from '../Container/container';
+import Search from './Search/search';
 
 import traveloLogo from '../../../public/assets/travelo-logo-header.png';
 
 const Header = () => {
 	return (
-		<header className="h-[85px] bg-white shadow-lg p-4">
+		<header className="h-[85px] bg-white shadow-lg p-4 w-screen">
 			<Container>
 				<div className="flex justify-between items-center">
-					<Image src={traveloLogo} alt="Travelo Logo" placeholder="blur" height="48" width="182" />
+					<div className="flex">
+						<Image src={traveloLogo} alt="Travelo Logo" placeholder="blur" height="48" width="182" />
+						<Search />
+					</div>
 				</div>
 			</Container>
 		</header>
