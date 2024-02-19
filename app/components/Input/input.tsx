78f1fb19e9fd36a3fa6fc5d29fe4cb8b426edc30
LAPTOP-K2PCPM3S\LENOVO <anandarizky166@gmail.com>
2024-02-19 21:31:42 +0700

    input component

---
+++ app/components/Input/input.tsx
@@ -0,0 +1,16 @@
+import clsx from 'clsx';
+
+import type { InputProps } from './interface';
+import styles from './input.module.css';
+
+const Input = ({ name, type, label, placeholder, className }: InputProps) => {
+	return (
+		<label htmlFor={name} className={clsx(styles.label, className)}>
+			<div className="mb-3">{label}</div>
+
+			<input type={type} name={name} id={name} placeholder={placeholder} className={styles.input} />
+		</label>
+	);
+};
+
+export default Input;
