97b3eb5c9dd5e8d7332ae9fdc3aa8424098f6912
DESKTOP-MAF5T6N\Darky <anandarizky166@gmail.com>
2024-02-20 16:50:05 +0700

    improve input component

--- app/login/Input/input.tsx
+++ app/login/Input/input.tsx
@@ -7,12 +7,16 @@ import { FaEye, FaEyeSlash } from 'react-icons/fa';
 import type { InputProps } from './interface';
 import styles from './input.module.css';
 
-const Input = ({ name, type, label, placeholder, className }: InputProps) => {
+const Input = ({ name, type, label, placeholder, className, register, errors, required, pattern }: InputProps) => {
 	// Use state untuk merubah kondisi hide password
 	const [hidePassword, setHidePassword] = useState(false);
 
 	// Use state untuk merubah type
 	const [inputType, setInputType] = useState(type);
+
+	const isInputError = errors && errors[name];
+
+	const inputErrorHint = `${name}-input-error-hint`;
 
 	const handleEyeClick = () => {
 		// Merubah kondisi dari false menjadi true
@@ -32,13 +36,26 @@ const Input = ({ name, type, label, placeholder, className }: InputProps) => {
 		<label htmlFor={name} className={clsx(styles.label, className)}>
 			<div className="mb-3">{label}</div>
 
-			<input type={inputType} name={name} id={name} placeholder={placeholder} className={styles.input} />
+			<input
+				{...register(name, {
+					required,
+					pattern,
+				})}
+				formNoValidate
+				type={inputType}
+				id={name}
+				placeholder={placeholder}
+				className={styles.input}
+				aria-describedby={inputErrorHint}
+			/>
 
 			{type === 'password' ? (
-				<button type="button" className={styles.eye} onClick={handleEyeClick}>
+				<button type="button" className={clsx(styles.eye, isInputError && styles.eyeError)} onClick={handleEyeClick}>
 					{hidePassword ? <FaEyeSlash className="h-6 w-6 text-gray-100" /> : <FaEye className="h-6 w-6 text-gray-100" />}
 				</button>
 			) : null}
+
+			{isInputError ? <span className="text-xs text-red-100 mt-1 ml-3">{errors[name].message}</span> : null}
 		</label>
 	);
 };
--- app/login/Input/interface.ts
+++ app/login/Input/interface.ts
@@ -1,9 +1,14 @@
-import type { InputHTMLAttributes } from "react";
+import type { InputHTMLAttributes } from 'react';
+import type { UseFormRegister, FieldError, ValidationRule } from 'react-hook-form';
 
 export type InputProps = {
-  name: InputHTMLAttributes<HTMLInputElement>['name']
-  type: InputHTMLAttributes<HTMLInputElement>['type']
-  label: string
-  placeholder: string
-  className?: string
-}
+	name: InputHTMLAttributes<HTMLInputElement>['name'] | string;
+	type: InputHTMLAttributes<HTMLInputElement>['type'];
+	label: string;
+	placeholder: string;
+	className?: string;
+  register: UseFormRegister<any>
+  errors?: FieldError
+  required?: string | ValidationRule<boolean>
+  pattern?: ValidationRule<RegExp>
+};
